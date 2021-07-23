5737e6b92fe1dc59604101241c639a067206b77d
Edixon van Vliet <edixon.vanvliet@hotmail.com>
2021-07-22 21:14:20 -0400

    Agregar los repositorios

--- POS.Data/UnitOfWork/Interfaces/IUnitOfWork.cs
+++ POS.Data/UnitOfWork/Interfaces/IUnitOfWork.cs
@@ -2,6 +2,12 @@ namespace POS.Data
 {
     public interface IUnitOfWork
     {
-
+        public IRepository<Color> Colors { get; set; }
+        public IRepository<Product> Products { get; set; }
+        public IRepository<Purchase> Purchases { get; set; }
+        public IRepository<Sale> Sales { get; set; }
+        public IRepository<Size> Sizes { get; set; }
+        public IRepository<Supplier> Suppliers { get; set; }
+        public IRepository<Type> Types { get; set; }
     }
 }
--- POS.Data/UnitOfWork/UnitOfWork.cs
+++ POS.Data/UnitOfWork/UnitOfWork.cs
@@ -1,9 +1,31 @@
+using System.Threading.Tasks;
+
 namespace POS.Data
 {
     public class UnitOfWork : IUnitOfWork
     {
+        private readonly ApplicationDbContext _context;
         public UnitOfWork(ApplicationDbContext context)
         {
+            _context = context;
+
+            Colors = new Repository<Color>(context);
+            Products = new Repository<Product>(context);
+            Purchases = new Repository<Purchase>(context);
+            Sales = new Repository<Sale>(context);
+            Sizes = new Repository<Size>(context);
+            Suppliers = new Repository<Supplier>(context);
+            Types = new Repository<Type>(context);
         }
+
+        public async Task SaveChangesAsync() => await _context.SaveChangesAsync();
+
+        public IRepository<Color> Colors { get; set; }
+        public IRepository<Product> Products { get; set; }
+        public IRepository<Purchase> Purchases { get; set; }
+        public IRepository<Sale> Sales { get; set; }
+        public IRepository<Size> Sizes { get; set; }
+        public IRepository<Supplier> Suppliers { get; set; }
+        public IRepository<Type> Types { get; set; }
     }
 }
