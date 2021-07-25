6c35f7a2a76f866d8906b8bd6767f8f331822d2f
Edixon van Vliet <edixon.vanvliet@hotmail.com>
2021-07-25 04:02:15 -0400

    Manejar articulos y menu lateral

--- POS/Controllers/HomeController.cs
+++ POS/Controllers/HomeController.cs
@@ -1,27 +1,48 @@
 ﻿using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using POS.Data;
 using POS.Models;
+using POS.Service;
 
 namespace POS.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IProductService _productService;
+        private readonly IService<Type> _typeService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IProductService productService, IService<Type> typeService)
         {
             _logger = logger;
+            _productService = productService;
+            _typeService = typeService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index([FromQuery] int typeId)
         {
             if (User.IsInRole("Admin"))
             {
                 return RedirectToAction("Index", "Product");
             }
 
-            return View();
+            var types = await _typeService.GetAllAsync();
+
+            if (typeId == default(int) && types.Any())
+            {
+                typeId = types.First().Id;
+                return RedirectToAction(nameof(Index), new { typeId = types.First().Id });
+            }
+
+            ViewData["typeId"] = typeId;
+            ViewData["Types"] = types;
+            var productos = await _productService.GetByTypeAsync(typeId);
+
+            return View(productos);
         }
 
         public IActionResult Privacy()
